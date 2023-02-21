63462ac86c81cbc78bbf2d34d794b83eb5fa72ec
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-21 02:11:13 -0800

    Update Source component import

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -4,12 +4,12 @@ import Source from "./Source";
 export default function WordEntry(props) {
   return (
     <div className="word-entry">
-      <Source sourceUrl={props.item.sourceUrls} />
       <WordEntryHeader
         word={props.wordData.word}
         phonetic={props.wordData.phonetic}
       />
       {props.meaningElements}
+      {props.wordData && <Source wordData={props.wordData} />}
     </div>
   );
 }
