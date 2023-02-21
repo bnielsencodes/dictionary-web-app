90f206a0e2dd9e53f2b7252efe43ec8002930d9c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 22:08:20 -0800

    Map prop to add definitions data

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,10 +1,15 @@
+export default function Meaning(props) {
+  const definitions = props.item.definitions.map((item) => {
+    return <li>{item.definition}</li>;
+  });
+
   return (
     <div className="meaning">
       <div className="box">
         <p className=""></p>
       </div>
       <p className="">Meaning</p>
-      <ul className=""></ul>
+      <ul className="">{definitions}</ul>
       <dl className="synonyms">
         <dt>
           <p className="">Synonyms</p>
