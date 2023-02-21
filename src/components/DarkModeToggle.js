ec4e565b6b87d8758e3b7bec7ea06a8cbb71bb67
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 22:15:29 -0800

    Pass props

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,9 +1,9 @@
 import moonIcon from "../assets/images/icon-moon.svg";
 
-export default function DarkModeToggle() {
+export default function DarkModeToggle(props) {
   return (
     <div className="dark-mode-toggle">
-      <div className="toggler--slider">
+      <div className="toggler--slider" onClick={props.toggleDarkMode}>
         <div className="toggler--slider--circle"></div>
       </div>
       <img src={moonIcon} alt="moon icon" />
