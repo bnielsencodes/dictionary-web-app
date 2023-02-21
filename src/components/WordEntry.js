045690b5eb1886d9ecae64212a3f64612cd8de2d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 22:06:30 -0800

    Map prop and import Meanings component

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -1,10 +1,22 @@
-export default function WordEntry() {
 import WordEntryHeader from "./WordEntryHeader";
+import Meaning from "./Meaning";
 import Source from "./Source";
+
+export default function WordEntry(props) {
+  const meanings = props.item.meanings.map((item) => {
+    return (
+      <Meaning
+        key={item.id}
+        item={item}
+      />
+    );
+  });
+
   return (
     <div className="word-entry">
       <WordEntryHeader />
       <Source />
+      {meanings}
     </div>
   );
 }
