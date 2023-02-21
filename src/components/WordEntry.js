b75ca00e3622f055c131b65b0211f4302e9b8a11
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 21:55:32 -0800

    Import Source component

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -1,8 +1,10 @@
 export default function WordEntry() {
 import WordEntryHeader from "./WordEntryHeader";
+import Source from "./Source";
   return (
     <div className="word-entry">
       <WordEntryHeader />
+      <Source />
     </div>
   );
 }
