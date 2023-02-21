7a50aa07436e6d2173e7f137a2822218689a47c5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 22:15:07 -0800

    Pass props

--- src/components/WordEntryHeader.js
+++ src/components/WordEntryHeader.js
@@ -1,8 +1,8 @@
-export default function WordEntry() {
+export default function WordEntry(props) {
   return (
     <div className="word-entry">
-      <p className="word-entry--heading"></p>
-      <p></p>
+      <p className="word-entry--heading">{props.word}</p>
+      <p>{props.phonetic}</p>
     </div>
   );
 }
