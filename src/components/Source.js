e2fa565b709922c87e87f013d58fbb337340ae09
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-21 02:11:27 -0800

    Update props

--- src/components/Source.js
+++ src/components/Source.js
@@ -2,7 +2,7 @@ export default function Source(props) {
   return (
     <div className="source">
       <p>Source</p>
-      <a href={props.sourceUrl[0]}>{props.sourceUrl}</a>
+      <a href={props.wordData.src}>{props.wordData.src}</a>
     </div>
   );
 }
