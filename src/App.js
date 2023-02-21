3decd25859d3805c32c0bb29ecc92a8bf9ea8e3a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-02-20 21:30:01 -0800

    Create states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+  const [searchInput, setSearchInput] = useState({
+    searchInput: "",
+  });
+  const [searchedWord, setSearchedWord] = useState([]);
+  const [wordData, setWordData] = useState([]);
+
 
   return (
     <div className="main">
