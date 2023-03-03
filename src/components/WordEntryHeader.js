2a78a77e8343ddf22e71fa055cd488238fa4bc88
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-03 14:41:08 -0800

    Delete WordEntryHeader.js

--- src/components/WordEntryHeader.js
+++
@@ -1,8 +0,0 @@
-export default function WordEntry(props) {
-  return (
-    <div className="word-entry">
-      <p className="word-entry--heading">{props.word}</p>
-      <p>{props.phonetic}</p>
-    </div>
-  );
-}
