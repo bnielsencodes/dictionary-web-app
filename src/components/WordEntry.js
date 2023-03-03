dee011c609614c22cd917bd4204bfea8d353c67e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-03 14:49:52 -0800

    Reformat WordEntryHeader component

--- src/components/WordEntry.js
+++ src/components/WordEntry.js
@@ -1,13 +1,12 @@
-import WordEntryHeader from "./WordEntryHeader";
 import Source from "./Source";
 
 export default function WordEntry(props) {
   return (
     <div className="word-entry">
-      <WordEntryHeader
-        word={props.wordData.word}
-        phonetic={props.wordData.phonetic}
-      />
+      <div>
+        <p className="word-entry--heading">{props.wordData.word}</p>
+        <p>{props.wordData.phonetic}</p>
+      </div>
       {props.meaningElements}
       {props.wordData && <Source wordData={props.wordData} />}
     </div>
