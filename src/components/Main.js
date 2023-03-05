cadfd6c7e0271a166b7bbafcd1a3ba002547ef5c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-04 18:19:35 -0800

    Reformat structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,13 @@ import Source from "./Source";
 export default function Main(props) {
   return (
     <div className="main">
-      <div>
+      <div className="main--inner-container">
+        <div className="main--header">
           <p className="main--heading">{props.wordData.word}</p>
           <p className="main--phonetic">{props.wordData.phonetic}</p>
+        </div>
+        {props.meaningElements}
       </div>
-      {props.meaningElements}
       {props.wordData && <Source wordData={props.wordData} />}
     </div>
   );
