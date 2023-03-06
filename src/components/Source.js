2e98349808163665f8f1f59aa38e1c8b6c43da47
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-05 19:56:57 -0800

    Update classes

--- src/components/Source.js
+++ src/components/Source.js
@@ -2,7 +2,7 @@ import iconNewWindow from "../assets/images/icon-new-window.svg";
 
 export default function Source(props) {
   return (
-    <div className="source">
+    <div className={props.darkMode ? "source dark" : "source"}>
       <p>Source</p>
       <div>
         <a href={props.wordData.src}>{props.wordData.src}</a>
