c9be8c458cb4538751204631b2073d0f62a7cc13
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-05 20:01:33 -0800

    Create Loader.js

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import spinner from "../assets/images/loader-regular.svg";
+
+export default function Loader() {
+  return (
+    <div className="loader">
+      <img
+        src={spinner}
+        alt=""
+        aria-hidden="true"
+        className="loader__spinner"
+      />
+    </div>
+  );
+}
