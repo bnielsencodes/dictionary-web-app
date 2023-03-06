f7ad6c4a03d3ecf804f2b3aaae6a85e8c8dde675
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-05 19:57:36 -0800

    Add styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import Source from "./Source";
 
 export default function Main(props) {
+  const borderLightStyles = {
+    borderBottom: props.wordData ? "1px solid #e9e9e9" : "0",
+  };
+
+  const borderDarkStyles = {
+    borderBottom: props.wordData ? "1px solid #383838" : "0",
+  };
+
   return (
     <div className="main">
       <div className="main--inner-container">
+        style={props.darkMode ? borderDarkStyles : borderLightStyles}
         <div className="main--header">
           <p className="main--heading">{props.wordData.word}</p>
           <p className="main--phonetic">{props.wordData.phonetic}</p>
