89eeeffd9411857a51234eaa1c9741fa918a120e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-06 21:12:19 -0800

    Update props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,17 @@ import searchIcon from "../assets/images/icon-search.svg";
 
 export default function SearchBar(props) {
   return (
-      <form onSubmit={props.handleSubmit}>
     <div className={props.darkMode ? "search-bar dark" : "search-bar"}>
+      <form noValidate onSubmit={props.validateInput}>
         <input
           id="search"
           className="search-bar__input"
           type="text"
-          placeholder="Search for any word..."
-          onChange={props.handleSearchChange}
           name="searchInput"
           value={props.searchInput.searchInput}
+          data-valid={props.valid}
+          placeholder="Search for any word..."
+          onChange={props.handleSearchChange}
         />
         <label htmlFor="search-bar__input" className="search-bar__label">
           <button>
