d62d03fe3676a304c216d18d9efab51acdf9b0a5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-03-06 17:22:06 -0800

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,14 +3,17 @@ import emojiIcon from "../assets/images/icons8-confused-face-96.png";
 
 export default function NotFound(props) {
   return (
-    <div className="not-found">
-      <img className="not-found__icon" src={emoji} alt="" aria-hidden="true" />
     <div className={props.darkMode ? "not-found dark" : "not-found"}>
+      <img
+        className="not-found__icon"
         src={emojiIcon}
+        alt="confused emoji face icon"
+        aria-hidden="true"
+      />
       <h1 className="not-found__title">No Definitions Found</h1>
       <p className="not-found__message">
-        Sorry pal, we couldn't find definitions for the word you were looking
-        for. You can try the search again at later time or head to the web
+        Sorry, pal. We couldn't find definitions for the word you were looking
+        for. You can try the search again at a later time or head to the web
         instead.
       </p>
     </div>
